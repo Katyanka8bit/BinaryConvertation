8fec676065dae5229a223ee9d9998e260edb9a93
Katyanka <katrinasosedova2013@yandex.ru>
2021-08-17 11:51:44 +0300

    Add new metod BinaryConverter

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        getBinaryNumber(125);
+        getBinaryNumber(25);
+        Integer result = BinaryConverter(25);
+        System.out.println(result);
 
     }
 
@@ -39,7 +41,30 @@ public class Main {
         }
         System.out.println(", наибольшее количество нулей = " + maxLength);
     }
+
+    public static Integer BinaryConverter(Integer number) {
+        int length = 0, maxLength = 0;
+
+        while (number > 1) {
+
+            if (number % 2 == 0) {
+                length++;
+                if (length > maxLength)
+                    maxLength++;
+            } else {
+                length = 0;
+            }
+            number = number / 2;
+        }
+
+        if (number % 2 == 0)
+            maxLength++;
+        return maxLength;
+    }
+
 }
+
+
 
 
 
